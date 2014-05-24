afa7791c94ed1416373af62891fe2e1f5e96cf59
Niklas Modess <niklas.modess@gmail.com>
2014-05-24 16:53:28 +0200

    Aggregate statitics for hours of day

---
+++ lib/Gitter/Statistics/Hour.php
@@ -0,0 +1,26 @@
+<?php
+namespace Gitter\Statistics;
+
+use Gitter\Util\Collection;
+use Gitter\Model\Commit\Commit;
+
+/**
+ * Aggregate statistics based on hour
+ */
+class Hour extends Collection implements StatisticsInterface
+{
+    /**
+     * @param Commit $commit
+     */
+    public function addCommit(Commit $commit)
+    {
+        $hour = $commit->getCommiterDate()->format('H');
+
+        $this->items[$hour][] = $commit;
+    }
+
+    public function sortCommits()
+    {
+        ksort($this->items);
+    }
+}
